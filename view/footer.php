c1068fa399db8b883f9c944dfd86f6eed3ebb358
Ivan <ivanfullstackdev@gmil.com>
2018-03-06 17:36:18 +0800

    creating the header file and update the style file

---
+++ view/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer_title">
+        <h5>Ivan Pratama &copy;2018 All bytes Reserved</h5>
+    </div>
+</footer>
---
+++ view/header.php
@@ -0,0 +1,6 @@
+<header>
+        <div class="theTitle">
+            <h1>Sistem Peminjaman buku di Perpustakaan</h1>
+            <h4>Just for Fun</h4>
+        </div>
+</header>
