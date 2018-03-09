097a802b41179aef337648151566f88e3409734b
Ivan <ivanfullstackdev@gmil.com>
2018-03-09 17:19:43 +0800

    paste the query file from the select_member to this file

---
+++ view/admin/crudMember/data_selectM.php
@@ -0,0 +1,38 @@
+<?php
+        include_once("../../../proccess/connect.php");
+        $sql = "SELECT * FROM tb_anggota";
+        $result = mysqli_query($conn,$sql);
+        //var_dump($result);
+        if(mysqli_num_rows($result) > 0){ ?>
+        
+        <div class="tb-container">
+            <div class="tb-title">
+                <h2>Anggota Perpustakaan</h2>
+                <button class="btn-tambah-data">Tambah</button>
+            </div>
+            <table class="tb-select">
+                <tr>
+                    <th>Id Anggota</th>
+                    <th>Nama Anggota</th>
+                    <th>Password Anggota</th>
+                    <th>Alamat Anggota</th>
+                    <th>Telp Anggota</th>
+                    <th>Edit</th>
+                    <th>Hapus</th>
+
+                </tr>
+                <?php 
+                while($row = mysqli_fetch_array($result)){ ?>
+                        <tr>
+                            <td><?php echo $row[0];?></td>
+                            <td><?php echo $row[1];?></td>
+                            <td><?php echo $row[2];?></td>
+                            <td><?php echo $row[3];?></td>
+                            <td><?php echo $row[4];?></td>
+                            <td><a class="btn_update" href="#">Edit</a></td>
+                            <td><a href="#">Hapus</a></td>
+                        </tr>
+  <?php         } 
+        } ?>
+            </table>
+        </div>  
