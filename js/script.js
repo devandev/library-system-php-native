69c718b7c9aded37cacd4ebbedb832a9d61f971e
Ivan <ivanfullstackdev@gmil.com>
2018-03-09 17:13:55 +0800

    change the function of the member

--- js/script.js
+++ js/script.js
@@ -2,6 +2,23 @@ $(document).ready(function(){
     var btnClose = $(".btn_close");
     var btnDaftar = $("#btn-daftar");
     var btnLogout = $("#btn_logout");
+    var btnTambah = $(".btn-tambah-data");
+    var btnUpdate = $(".btn_update");
+
+    loaddata();
+
+
+    //for crud
+    $(document).on("click", ".btn-tambah-data", function(){
+        $(".background-form").show();
+    });
+
+
+    $(document).on("click", ".btn_update", function(){
+        $(".background-form-up").show();
+    });
+
+    
 
     btnClose.on("click", function(){
         $(".background-form").hide();
@@ -12,6 +29,28 @@ $(document).ready(function(){
         $(".background-form").show();
         $("#daftar").show();
     });
+
+
+    function loaddata(){
+        $.get("data_selectM.php", function(data){
+            $(".container").html(data);
+        });
+    }
+
+
+    var btnAddM = $("#btn_submit");
+    btnAddM.on("submit", function(e){
+
+        e.preventDefault();
+    });
+
+
+    //for ajax
+
+
+
+
+
 
 
 
