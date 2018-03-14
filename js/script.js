36bee717af3d45aa18ad53591158aa31ab84b415
Ivan <ivanfullstackdev@gmil.com>
2018-03-14 19:55:40 +0800

    adding btn update functuion  and edit the some code

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     var btnTambah = $(".btn-tambah-data");
     var btnUpdate = $(".btn_update");
 
-   
 
     /*==========================
     CRUD MEMBER
@@ -37,20 +36,52 @@ $(document).ready(function(){
         $(".background-form").hide();
     });
 
-    //create event click for update form
+    //get value to display inside input update form
     $(document).on("click", ".btn_update", function(){
-        $(".background-form-up").show();
+        var id_anggota = $(this).attr("id");
+        
+        var currentRow=$(this).closest("tr"); 
+         var col1=currentRow.find("td:eq(0)").text(); // get current row 1st TD value
+         var col2=currentRow.find("td:eq(1)").text(); // get current row 2nd TD
+         var col3=currentRow.find("td:eq(2)").text();
+         var col4=currentRow.find("td:eq(3)").text();
+         var col5=currentRow.find("td:eq(4)").text();
+         
+         $("#id_anggota").val(col1);
+         $("#nama_anggota").val(col2);
+         $("#password_anggota").val(col3);
+         $("#alamat_anggota").val(col4);
+         $("#telp_anggota").val(col5);
+         $(".background-form-up").show();
     });
 
-    $(".update-form").on("submit",function(e){
+    $("#update-form").on("submit",function(e){
+        // $.ajax({
+        //     url : $(this).attr("action"),
+        //     type : $(this).attr("method"),
+        //     data : $(this).serialize(),
+        //     success : function(){
+        //         loaddata();
+        //     }
+        // });
         $.ajax({
-            url : $(this).attr("action"),
-            type : $(this).attr("method"),
-            data : $(this).serialize(),
-            success : function(){
-                loaddata();
+            type:'post',
+            url : 'data_selectMDt.php',// The file where my php code is
+            data:{
+                // all variables i want to pass. to php file data_selectMDt.php
+                'id':col1, 
+                'nama':col2,
+                'password':col3,
+                'alamat':col4,
+                'telp':col5
+            },
+            success:function(data){ // in case of success get the output, i named data
+                loaddata(); // do something with the output, like an alert
             }
-        });
+         });
+        
+         
+
         e.preventDefault();
         $(".background-form-up").hide();
     });
